2fc27c112025392ec9f5dae710ac7757adc5f9b6
Agnieszka Palmowska <a.palmowska@freshpixels.pl>
2023-01-11 00:29:38 +0100

    new HP layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,20 @@ module.exports = {
             fontFamily: {
                 "palmiak-font": ['"JetBrainsMono"', "serif"],
                 "palmiak-font-serif": ['"JetBrainsMono"', "serif"],
+                "palmiak-font-sansserif": ['"Poppins"', "sans-serif"],
             },
 
             colors: {
-                "palmiak_pink": "#F027A6",
+                "palmiak_pink": "#EA4C88",
                 "palmiak_blue": "#16E6F3",
-                "palmiak_green": "#A05FE0",
-                "palmiak_bg": "#2b213a"
+                "palmiak_green": "#35DAE5",
+                "palmiak_purple": "#3F2DB9",
+                "palmiak_bg": "#1D1F57",
+                "palmiak_dark_bg": "#151738"
+            },
+            lineHeight: {
+                'h1': '130%',
+                'h2': '120%',
             },
         },
     },
